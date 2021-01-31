9dda21c2c0cfb85bf89df5c4f6645ad3d3da4f4a
黃敏雄 <64833384+MinXiong-Huang@users.noreply.github.com>
2021-01-31 11:42:12 +0800

    Update Socket_Chat_Client.py

--- Socket_Chat_Client.py
+++ Socket_Chat_Client.py
@@ -2,10 +2,10 @@ import socket
 
 while True:
     Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #建立Socket
-    Host_IP='127.0.0.1' #主機IP
-    Host_Port=12345 #主機Port
+    Host_IP = '127.0.0.1' #主機IP
+    Host_Port = 12345 #主機Port
     Socket.connect((Host_IP,Host_Port)) #連接Server
-    message=input('請輸入訊息:')
+    message = input('請輸入訊息:')
     Socket.send(message.encode()) #訊息加碼
     print("我說:",Socket.recv(1024).decode()+'\n') #數據解碼
     
